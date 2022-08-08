1e056c3b35fb92998df9cfce7c04814763d62cff
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2022-08-08 14:18:26 +0900

    [ADD] 게시물 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let blogName = "정현 블로그";
+  let [글제목, b] = useState(["서울 맛집", "남양주 맛집", "코틀린 스프링부트"]);
+  const blogName = "정현 블로그";
+
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그임</h4>
+        <h4>{blogName}</h4>
       </div>
-      <h4>{blogName}</h4>
+      <div className="list">
+        <h4>{글제목[0]}</h4>
+        <p>8월 8일 발행</p>
+      </div>
     </div>
   );
 }
