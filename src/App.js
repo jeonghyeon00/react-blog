384e3c01675ad2e974e84da9e62f24a89714d686
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2022-08-08 13:49:23 +0900

    [ADD] 블로그 제목 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let blogName = "정현 블로그";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>블로그임</h4>
+      </div>
+      <h4>{blogName}</h4>
     </div>
   );
 }
