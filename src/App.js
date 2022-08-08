b09df0a1b6daaa4993f343b6c6ea143ea7b4e00c
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2022-08-08 14:25:57 +0900

    [ADD] 좋아요 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
   let [글제목, b] = useState(["서울 맛집", "남양주 맛집", "코틀린 스프링부트"]);
+  let [좋아요, 좋아요변경] = useState(0);
   const blogName = "정현 블로그";
 
   return (
@@ -11,7 +12,24 @@ function App() {
         <h4>{blogName}</h4>
       </div>
       <div className="list">
-        <h4>{글제목[0]}</h4>
+        <h4>
+          {글제목[0]}{" "}
+          <span
+            onClick={() => {
+              좋아요변경(좋아요 + 1);
+            }}
+          >
+            👍 {좋아요}
+          </span>{" "}
+        </h4>
+        <p>8월 8일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>8월 8일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
         <p>8월 8일 발행</p>
       </div>
     </div>
